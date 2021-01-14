f09dc7f22e08bc01a451d1799e5b47bb79b6708c
Sung Hwan (Justin) Yoon <jshyoon95@gmail.com>
2021-01-14 15:36:58 -0500

    Update etl.py

--- etl.py
+++ etl.py
@@ -72,7 +72,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = (DEFAULT, row.ts, row.userId, row.level, songid, artistid, row.sessionId,\
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId,\
                      row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
